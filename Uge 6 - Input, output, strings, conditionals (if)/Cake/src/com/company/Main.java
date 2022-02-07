e8bb0969fa0124792bee2683f85f80d275e0271f
FHallengreen <fred70p9@stud.kea.dk>
2022-02-07 19:05:26 +0100

    V.2

--- Uge 6 - Input, output, strings, conditionals (if)/Cake/src/com/company/Main.java
+++ Uge 6 - Input, output, strings, conditionals (if)/Cake/src/com/company/Main.java
@@ -14,46 +14,48 @@ public class Main {
 
     // Vores ingredienser bliver defineret. Følgende svarer til gram per enkelte person.
 
-    double wheat = 35.0;
-    double egg = 30.0;
-    double vanilla = 0.06;
-    double sugar = 37.5;
+    double wheat    = 35.0;
+    double egg      = 30.0;
+    double vanilla  = 0.06;
+    double sugar    = 37.5;
 
     // Vi regner nu sammen hvor mange gram der skal bruges af de forskellige ingredienser.
 
-    double calculatedWheat = wheat *  amountOfPeople;
-    double calculatedEgg = egg * amountOfPeople;
-    double calculatedVanilla = vanilla * amountOfPeople;
-    double calculatedSugar = sugar * amountOfPeople;
+    double calculatedWheat    = wheat * amountOfPeople;
+    double calculatedEgg      = egg * amountOfPeople;
+    double calculatedVanilla  = vanilla * amountOfPeople;
+    double calculatedSugar    = sugar * amountOfPeople;
 
     // total vægt regnes sammen
     int calculatedTotal = (int) (calculatedEgg + calculatedVanilla + calculatedWheat + calculatedSugar);
 
     // vi udskriver hvor meget der skal bruges af de enkelte ingredienser.
+
     System.out.printf("For at lave Fredes Drømmekage til %s personer, skal du bruge: \n\t %5.2f "
             + "g hvede \n\t %5.2f g æg\n\t%5.2f g vanilje \n\t %5.2f g sukker \n",
         amountOfPeople, calculatedWheat, calculatedEgg, calculatedVanilla, calculatedSugar);
 
     //totalen udskrives
+
     System.out.println("\nDin kage skulle gerne veje cirka " + calculatedTotal + " gram.");
 
     /* Ekstra --ERNÆRINGSINDHOLD--
     // Laver et array til at definere ernæringsindholdet i følgende rækkefølge:
-    kalorier, Protein, Kulhydrater, Sukker & Fedt
+                            kalorier, Protein, Kulhydrater, Sukker & Fedt
     */
 
-    double[] wheatKj = new double[]{335.0, 16.0, 57.0, 0.7, 2.3};
-    double[] eggKj = new double[]{137.0, 12.0, 1.3, 0.0, 2.8};
-    double[] vanillaKj = new double[]{195.0, 4.6, 17.0, 10.7, 5.9};
-    double[] sugarKj = new double[]{400.0, 0.0, 100.0, 100.0, 0.0};
+    double[] wheatKj    =   {335.0, 16.0, 57.0, 0.7, 2.3};
+    double[] eggKj      =   {137.0, 12.0, 1.3, 0.0, 2.8};
+    double[] vanillaKj  =   {195.0, 4.6, 17.0, 10.7, 5.9};
+    double[] sugarKj    =   {400.0, 0.0, 100.0, 100.0, 0.0};
 
     // Alt regnes sammen (f.eks. alle kalorier under første array [0] osv.)
 
-    int sumCalories = (int) DoubleStream.of(wheatKj[0] + eggKj[0] + vanillaKj[0], sugarKj[0]).sum();
-    double sumProtein = DoubleStream.of(wheatKj[1] + eggKj[1] + vanillaKj[1], sugarKj[1]).sum();
-    double sumCarbs = DoubleStream.of(wheatKj[2] + eggKj[2] + vanillaKj[2], sugarKj[2]).sum();
-    double sumSugar = DoubleStream.of(wheatKj[3] + eggKj[3] + vanillaKj[3], sugarKj[3]).sum();
-    double sumFat = DoubleStream.of(wheatKj[4] + eggKj[4] + vanillaKj[4], sugarKj[4]).sum();
+    double sumCalories  = DoubleStream.of(wheatKj[0] + eggKj[0] + vanillaKj[0], sugarKj[0]).sum();
+    double sumProtein   = DoubleStream.of(wheatKj[1] + eggKj[1] + vanillaKj[1], sugarKj[1]).sum();
+    double sumCarbs     = DoubleStream.of(wheatKj[2] + eggKj[2] + vanillaKj[2], sugarKj[2]).sum();
+    double sumSugar     = DoubleStream.of(wheatKj[3] + eggKj[3] + vanillaKj[3], sugarKj[3]).sum();
+    double sumFat       = DoubleStream.of(wheatKj[4] + eggKj[4] + vanillaKj[4], sugarKj[4]).sum();
 
     System.out.println("\nKagen indeholder per 100 gram: \n\t" + sumCalories + " kcal \n\t" + sumProtein + " g protein\n\t"
         + sumCarbs + " g kulhydrater \n\t" + sumSugar + " g sukker \n\t" + sumFat + " g fedt \n");
@@ -61,7 +63,7 @@ public class Main {
     /* til sidst ønsker jeg at udregne det totale ernæringsindhold for hele kagen. Først tages calculatedTotal og divideres med 100,
     for at gøre det nemmere at regne totalen ud */
 
-    int totalGram = (calculatedTotal / 100);
+    int totalGram       = (calculatedTotal / 100);
 
     System.out.println("Det svarer til at kagen totalt har følgende ernæringsindhold: \n\t" + (sumCalories * totalGram)
         + " kcal \n\t" + (sumProtein * totalGram) + " g protein \n\t" + (sumCarbs * totalGram) + " g kulhydrater \n\t"
