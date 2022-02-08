6d23b8a6980f0f80c453074f473c2f07e2ff273b
FHallengreen <fred70p9@stud.kea.dk>
2022-02-08 14:56:51 +0100

    Programmering

--- Uge 6 - Input, output, strings, conditionals (if)/DetFjerdeMellemrum/src/com/company/Main.java
+++ Uge 6 - Input, output, strings, conditionals (if)/DetFjerdeMellemrum/src/com/company/Main.java
@@ -5,16 +5,26 @@ public class Main {
     public static void main(String[] args) {
 
         String potter = "Harry Potter and the Order of the Phoenix";
+        // Starter med at finde ud af hvor mange tegn der er totalt (41), da det kan bruges senere hen.
+        System.out.println("Længden af tegn er: " + potter.length());
 
+        // ved hjælp af index finder vi nu frem til det fjerde mellemrum.
         String mellemrum = " ";
         int firstIndex  = potter.indexOf(mellemrum);
         int secondIndex = potter.indexOf(mellemrum, firstIndex + 1);
         int thirdIndex  = potter.indexOf(mellemrum, secondIndex + 1);
         int fourthIndex = potter.indexOf(mellemrum, thirdIndex + 1);
-        int fithIndex   = potter.indexOf(mellemrum, fourthIndex+1);
-        System.out.println(fourthIndex);
+        int fifthIndex   = potter.indexOf(mellemrum, fourthIndex+1);
+        System.out.println("Det fjerde mellemrum kommer efter følgende antal tegn: " + fourthIndex);
 
-        String word        = potter.substring(fourthIndex,fithIndex);
-        System.out.printf("%s ",word);
+        // Vi ved så at fourthIndex starter på 20. For at få første og sidste cifre
+        String word         = potter.substring(fourthIndex,fifthIndex);
+        String firstWord    = potter.substring(21,22);
+
+        int lastWord        = potter.substring(fourthIndex,fifthIndex).length();
+        String WordLast     = word.substring(lastWord-1);
+        char O = 'O';
+        char r = 'r';
+        System.out.printf("%s \n %s %s \n",word, firstWord, WordLast);
     }
 }
---
+++ Uge 6 - Input, output, strings, conditionals (if)/codePair/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	  String navn1 = "Harry";
+    String navn2 = "Ron";
+    String navn3 = "Hermione";
+
+
+        System.out.printf("De første code points for " + navn1 + " er: " );
+    }
+}
