655599558464a80794b6a04037091dd33d082d28
Frederik Hallengreen <fred70p9@stud.kea.dk>
2022-02-08 09:38:08 +0100

    Programming

--- Uge 6 - Input, output, strings, conditionals (if)/Capitalize/src/com/company/Main.java
+++ Uge 6 - Input, output, strings, conditionals (if)/Capitalize/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	
+	String Sarcasm = "dU eR SøRme gOd TiL aT KODe";
+        System.out.println(Sarcasm.toLowerCase());
     }
 }
