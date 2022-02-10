9fe78317e1be0aa03765de39a4b30d8d4d83eefa
Frederik Hallengreen <fred70p9@stud.kea.dk>
2022-02-10 11:09:30 +0100

    Programmering

---
+++ Uge 5 Intro, programmering, variabler og operatorer/BMICalculator/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Locale;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.ENGLISH);
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Indtast din vægt og højde:");
+        System.out.print("Din Vægt i kg ");
+        double weight = sc.nextDouble();
+        System.out.print("Din Højde i meter ");
+        double height = sc.nextDouble();
+        double bmi = weight / (height * height);
+        System.out.println("Dit BMI er: " + bmi);
+
+        if (bmi < 18.5){
+            System.out.println("Du er undervægtig, Æd noget mad!");}
+        else if (bmi > 18.6 && bmi < 24.9){
+            System.out.println("Du er normalvægtig");}
+        else if (bmi > 24.9 && bmi < 39.9){
+            System.out.println("Du er overvægtig");}
+        else if (bmi > 40){
+            System.out.println("Du er meget overvægtig");}
+        if (bmi > 45 || bmi < 15){
+            System.out.println("Dobbelt tjek om du har indtastet korrekt data!");}
+    }}
--- Uge 6 - Input, output, strings, conditionals (if)/FindFemFejl/src/com/company/Main.java
+++ Uge 6 - Input, output, strings, conditionals (if)/FindFemFejl/src/com/company/Main.java
@@ -4,27 +4,31 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        Random random = new Random();
-        int randomNumber = random.nextInt(1,6);
+      Random random = new Random();
+        int randomNumber = random.nextInt(6) + 1;
         System.out.println(randomNumber);
 
         int count = 0;
         System.out.println(++count);
-
+        1
         double count2 = 5;
         if (count2 >= 10) {
             System.out.println("Tal er større eller lig med 10");
     }
 
         double number1 = 50;
-        double number2 = 7;
+        int number2 = 7;
         double resultat = number1 / number2;
-        if (resultat > 7) {
+        if (resultat >= 7) {
             System.out.println("Resultat større end 7");
         }
-
+/*
         int number3 = 10;
         if (number3 == 10) {
             System.out.println("Variablen number indeholder værdien 10");
         }
-}}
+*/
+        int Nummer = 9;
+        if(Nummer == 8){
+            System.out.println(Nummer);
+}}}
