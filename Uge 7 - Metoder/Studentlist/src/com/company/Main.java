510fa1d714c8b32983bdb59294628bf972f9884f
FHallengreen <fred70p9@stud.kea.dk>
2022-02-16 16:12:07 +0100

    code

--- Uge 7 - Metoder/Studentlist/src/com/company/Main.java
+++ Uge 7 - Metoder/Studentlist/src/com/company/Main.java
@@ -6,7 +6,31 @@ import java.util.Scanner;
 
 public class Main {
 
+    public String capitalize(String name) {
+        String capital = name.substring(0, 1).toUpperCase();
+        String lowercase = name.substring(1).toLowerCase();
+        String correct = capital + lowercase;
+        return correct;
+    }
+
+    public String splittyMacSplit(String name) {
+        String space = " ";
+        int firstIndex = name.indexOf(space);
+        String lastName = name.substring(0, firstIndex - 1);
+
+        int lastIndex = name.indexOf(space);
+        String firstName = name.substring(lastIndex + 1);
+
+        return capitalize(firstName) + " " + capitalize(lastName);
+
+    }
+
+    public String getFullName(String lastName, String firstName) {
+
+        return splittyMacSplit(firstName) + " " + splittyMacSplit(lastName);
+    }
     public static void main(String[] args) throws FileNotFoundException {
+        Main obj = new Main();
         File file = new File("students.txt");
 
         try {
@@ -15,12 +39,11 @@ public class Main {
 
             while (sc.hasNextLine()) {
                 String i = sc.nextLine();
-                System.out.println(i);
+                System.out.println(obj.splittyMacSplit(i));
             }
             sc.close();
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
-
     }
 }}
