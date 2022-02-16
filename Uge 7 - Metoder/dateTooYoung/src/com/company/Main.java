132a6fd4cd05da7aaddf77186ba56d4a79330346
Frederik Hallengreen <fred70p9@stud.kea.dk>
2022-02-16 11:32:18 +0100

    code

--- Uge 7 - Metoder/dateTooYoung/src/com/company/Main.java
+++ Uge 7 - Metoder/dateTooYoung/src/com/company/Main.java
@@ -3,18 +3,21 @@ package com.company;
 import java.util.Locale;
 import java.util.Scanner;
 public class Main {
+public boolean tooYoung (int yourAge, int dateAge){
 
+return false;
+}
     public static void main(String[] args) {
         Scanner sc = (new Scanner(System.in)).useLocale(Locale.ENGLISH);
         new Scanner(System.in);
         System.out.println("Lad os tjekke om daten er for ung!?");
         System.out.print("Skriv din alder: ");
-        double yourAge = sc.nextDouble();
-        double ageOfYourDate;
-        double calculatedAge = (yourAge / 2 + 7);
+        int yourAge = sc.nextInt();
+        int ageOfYourDate;
+        int calculatedAge = (yourAge / 2 + 7);
         do {
             System.out.print("Indtast din dates alder: ");
-            ageOfYourDate = sc.nextDouble();
+            ageOfYourDate = sc.nextInt();
 
             if (ageOfYourDate > yourAge) {
                 System.out.println("Din date er ældre end dig");}
