aa194b482e79e71bad771d403c996b8f593ee2f0
Frederik Hallengreen <fred70p9@stud.kea.dk>
2022-02-22 11:37:28 +0100

    code

---
+++ Uge 8 - Loops og Arrays/Pixelpainter/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args)  throws IOException {
+	int width = 400;
+    int height = 300;
+
+    BufferedImage img = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+
+
+
+    }
+}
--- Uge 8 - Loops og Arrays/loopAssignments/src/com/company/Main.java
+++ Uge 8 - Loops og Arrays/loopAssignments/src/com/company/Main.java
@@ -6,11 +6,11 @@ import java.util.Random;
 
 public class Main {
 
-
     String firstname = "Frederik";
     char index = 'i';
     int findI;
     String mockify = "du er sørme god til at kode";
+    Random random = new Random();
 
     public void printOneLetter(){
 
@@ -40,7 +40,6 @@ public class Main {
 
     }
 
-    Random random = new Random();
     public void mockify(){
         for (int i = 0; i<=26;i++){
             String letter = mockify.substring(i, i+1);
@@ -55,12 +54,22 @@ public class Main {
         }
     }
 
+    public void starblock(int width, int height){
+        for (int x = 1; x<=height;x++){
+            for (int y = 1; y<=width ;y++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
     Main obj = new Main();
 //    obj.printOneLetter();
 //    obj.findIndexOf();
 //        obj.reverseString();
-        obj.mockify();
+//        obj.mockify();
+
+        obj.starblock(5,3);
     }
 /*	String navn = "Frederik Hansen";
         String fornavn = "";
