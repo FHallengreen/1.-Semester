5f28b79da2e5773d0a2af585859602eec64edfe9
Frederik Hallengreen <fred70p9@stud.kea.dk>
2022-02-25 14:15:19 +0100

    Revert "added AskAboutAge"
    This reverts commit 32cde5d2

--- Uge 8 - Loops og Arrays/gitExercise/src/com/company/Main.java
+++ Uge 8 - Loops og Arrays/gitExercise/src/com/company/Main.java
@@ -8,17 +8,10 @@ public class Main {
         System.out.println("Hej " + name);
 
     }
-
-    public void askAboutAge (String age){
-        System.out.println("Du er : " + age + " år gammel");
-        System.out.println("Stor ged");
-    }
     public static void main(String[] args) {
         System.out.println("It works... enter name: ");
         Main obj = new Main();
         Scanner scan = new Scanner(System.in);
         obj.greet(scan.nextLine());
-        System.out.println("Enter your age: ");
-        obj.askAboutAge(scan.nextLine());
     }
 }
