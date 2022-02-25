69e9c914a7238386728a8ca5c69a9a106e14f9ed
Frederik Hallengreen <fred70p9@stud.kea.dk>
2022-02-25 13:12:18 +0100

    added greet

--- Uge 8 - Loops og Arrays/gitExercise/src/com/company/Main.java
+++ Uge 8 - Loops og Arrays/gitExercise/src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public void greet (String name){
+        System.out.println("Hej " + name);
+
+    }
     public static void main(String[] args) {
-        System.out.println("It works");
+        System.out.println("It works... enter name: ");
+        Main obj = new Main();
+        Scanner scan = new Scanner(System.in);
+        obj.greet(scan.nextLine());
     }
 }
