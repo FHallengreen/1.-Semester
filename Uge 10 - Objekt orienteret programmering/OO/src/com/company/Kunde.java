506699bbc6449643a0a7b667fc6b5dd27bb46eb3
Frederik Hallengreen <fred70p9@stud.kea.dk>
2022-03-07 14:05:48 +0100

    OO

---
+++ Uge 10 - Objekt orienteret programmering/OO/src/com/company/Kunde.java
@@ -0,0 +1,15 @@
+package com.company;
+
+class Client {
+
+    String name;        // attribute
+    int age;         // attribute
+
+
+    //Constructor method
+
+    public Client(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
