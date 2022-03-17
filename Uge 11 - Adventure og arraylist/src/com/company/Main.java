c7c7ddbf8d37886c50b975409175581e37dfc427
Frederik Hallengreen <fred70p9@stud.kea.dk>
2022-03-17 16:46:02 +0100

    Arraylist

---
+++ Uge 11 - Adventure og arraylist/src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String [] names = {"Harry", "Ron", "Hermione"};
+        ArrayList<String> people = new ArrayList<>(List.of(names));
+        System.out.println(people);
+        people.add("Draco");
+        System.out.println(people);
+        people.add("Neville");
+        System.out.println(people);
+        people.remove("Draco");
+        System.out.println(people);
+        people.add("Luna");
+        System.out.println(people);
+        List youThree = people.subList(0,3);
+        System.out.println(youThree);
+        people.add(1,"Cho");
+        System.out.println(people);
+        people.add(1,"Ginny");
+        System.out.println(people);
+
+        people.addAll(List.of(new String [] {"Fred", "George"}));
+        System.out.println(people);
+
+        int result = people.indexOf("Fred");
+        people.remove(result);
+        System.out.println(people);
+    }
+}
