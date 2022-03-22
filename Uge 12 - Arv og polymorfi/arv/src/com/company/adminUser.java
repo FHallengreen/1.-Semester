0bb47f3ce59e28785dc09707260c969b8b5e0632
Frederik Hallengreen <fred70p9@stud.kea.dk>
2022-03-22 09:24:43 +0100

    arv

---
+++ Uge 12 - Arv og polymorfi/arv/src/com/company/adminUser.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class adminUser extends testUser {
+
+    public adminUser (String username, String password){
+        super(username,password);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
