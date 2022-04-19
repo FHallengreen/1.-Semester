c677972827caa0cfb3b8bc70c725f298dc333ba3
FHallengreen <fred70p9@stud.kea.dk>
2022-04-19 15:20:49 +0200

    update

--- Uge 14 - Kunder og brugere/examExercise2/src/com/company/Main.java
+++ Uge 14 - Kunder og brugere/examExercise2/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Film film1 = new Film("Skid mig i munden", 2019, new Producer("Jøde Kim"));
+        Film film1 = new Film("Tro på det", 2019, new Producer("Kim"));
         Film film2 = new Film("Der var engang");
         System.out.println(film1);
         System.out.println(film2);
